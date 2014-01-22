52279ecfeb9c689e6fda57c8131dcc15559a2ad8
Bret Stateham <Bret.Stateham@microsoft.com>
2014-01-21 19:21:51 -0800

    UI SIG

---
+++ UISIG/HelloWorld/HelloWorld_Phone8/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using HelloWorld_Phone8.Resources;
+
+namespace HelloWorld_Phone8
+{
+  /// <summary>
+  /// Provides access to string resources.
+  /// </summary>
+  public class LocalizedStrings
+  {
+    private static AppResources _localizedResources = new AppResources();
+
+    public AppResources LocalizedResources { get { return _localizedResources; } }
+  }
+}
